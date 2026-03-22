c412001891da5d14f5e1abb523e61c29c9f295ed
Cristina <cris.glez512@gmail.com>
2026-03-22 12:20:54 +0000

    añado el modelo y la migración

---
+++ jurassic-park/database/migrations/2026_03_22_121837_create_celdas_table.php
@@ -0,0 +1,32 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('celdas', function (Blueprint $table) {
+            $table->id();
+            $table->string('nombre');
+            $table->integer('cantidad_animales')->default(0);
+            $table->enum('nivel_peligrosidad', ['bajo', 'medio', 'alto', 'muy_alto', 'extremo', 'critico'])->default('bajo');
+            $table->integer('alimento')->default(0);
+            $table->enum('nivel_seguridad', ['bajo', 'medio', 'alto'])->default('medio');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('celdas');
+    }
+};
